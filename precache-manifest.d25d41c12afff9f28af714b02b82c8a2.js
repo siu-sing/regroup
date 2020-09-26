1f72f758ee7e214a0f1ab0eee793902ad42f4555
siu-sing <lai.siusing@gmail.com>
2020-09-27 01:18:51 +0800

    Updates

---
+++ precache-manifest.d25d41c12afff9f28af714b02b82c8a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6e62acbaed8cd1fbdacb2f1a0df8bf",
+    "url": "/regroup/index.html"
+  },
+  {
+    "revision": "15472ddd72c4353286d1",
+    "url": "/regroup/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e1f12874ea915bf7a79f",
+    "url": "/regroup/static/css/main.b6b4dbf8.chunk.css"
+  },
+  {
+    "revision": "15472ddd72c4353286d1",
+    "url": "/regroup/static/js/2.ac8aaead.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/regroup/static/js/2.ac8aaead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f12874ea915bf7a79f",
+    "url": "/regroup/static/js/main.7c34f770.chunk.js"
+  },
+  {
+    "revision": "d1b40839700ac6cd88c0",
+    "url": "/regroup/static/js/runtime-main.5663743c.js"
+  }
+]);
