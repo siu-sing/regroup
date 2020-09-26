3246667511b44f7968f831adf84d386fd2b3d35c
siu-sing <lai.siusing@gmail.com>
2020-09-27 01:15:53 +0800

    trying to fix deploy

--- src/nameList.js
+++ src/nameList.js
@@ -1,9 +1,13 @@
+
+// import faker from 'faker';
 var faker = require('faker');
 faker.seed(123);
-let nameList = [];
+let nameListTemp = [];
 
 [...Array(16).keys()].forEach(i => {
-    nameList.push(faker.name.findName())
+    nameListTemp.push(faker.name.findName())
 })
+
+const nameList = nameListTemp;
 
 module.exports = nameList;
